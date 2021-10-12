6d96c26b45b41db62b601b4d7146037612fed9df
Nicolas <vero.nicolas@yahoo.fr>
2021-10-12 17:24:11 +0200

    Update font.js

--- src/configs/design/font.js
+++ src/configs/design/font.js
@@ -9,6 +9,7 @@ export const loadFonts = async () =>{
       MontserratSemiBold: require('../../../assets/fonts/Montserrat-SemiBold.ttf'),
       Montserrat: require('../../../assets/fonts/Montserrat-Regular.ttf'),
       Roboto: require('../../../assets/fonts/Roboto-Regular.ttf'),
+      RobotoItalic: require('../../../assets/fonts/Roboto-Italic.ttf'),
       RobotoBold: require('../../../assets/fonts/Roboto-Bold.ttf'),
       RobotoMedium: require('../../../assets/fonts/Roboto-Medium.ttf'),
     });
